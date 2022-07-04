6803957d2d52c3e9e4c5dbcc5f48edaf7bd5b653
Petros Tsimvris <tsimvris@gmail.com>
2022-07-04 16:46:34 +0200

    added JS on bookmark and answer

---
+++ answer.js
@@ -0,0 +1,15 @@
+let answerButton = document.querySelector('[data-js="showAnswer"]');
+
+let answer = document.querySelector('[data-js="answer"]');
+
+function revealAnswer() {
+  answer.classList.toggle("showAnswer");
+}
+answerButton.addEventListener("click", revealAnswer);
+
+let bookmark = document.querySelector('[data-js="bookmark"]');
+let bmButton = document.querySelector('[data-js="bookmark-button"]');
+function isBookmarked() {
+  bookmark.classList.toggle("filterBlue");
+}
+bmButton.addEventListener("click", isBookmarked);
