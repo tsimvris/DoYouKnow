3cf89838c9984dd020c019f03a9d7fd595f1e8a8
Petros Tsimvris <tsimvris@gmail.com>
2022-07-05 17:21:34 +0200

    Add toggle on buttons name JS

--- JS/show-answer.js
+++ JS/show-answer.js
@@ -5,5 +5,10 @@ let answer = document.querySelector('[data-js="answer"]');
 
 function revealAnswer() {
   answer.classList.toggle("showAnswer");
+  if (answerButton.innerHTML === "Show Answer") {
+    answerButton.innerHTML = "Hide Answer";
+  } else {
+    answerButton.innerHTML = "Show Answer";
+  }
 }
 answerButton.addEventListener("click", revealAnswer);
