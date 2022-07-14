4c2819ed97e416a2638989f5540c62fbfa87506c
Petros Tsimvris <tsimvris@gmail.com>
2022-07-14 15:22:55 +0200

    add linting

--- JS/card.js
+++ JS/card.js
@@ -4,7 +4,7 @@ export default function FetchAndBuildCard() {
   const main = document.querySelector('[data-js="main"]');
   // FETCHING
   const urlToFetch =
-    "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"; // only 5 questions
+    'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple'; // only 5 questions
   async function fetchQuestionsFromApi() {
     try {
       const res = await fetch(urlToFetch);
@@ -21,66 +21,66 @@ export default function FetchAndBuildCard() {
 
   //CREATING CARDS
   function createCards(data) {
-    data.forEach((datensatz) => {
-      const element = document.createElement("div");
-      element.classList.add("card");
+    data.forEach(datensatz => {
+      const element = document.createElement('div');
+      element.classList.add('card');
       main.append(element);
 
       /* create card header*/
-      const header = document.createElement("div");
-      header.classList.add("card-top");
+      const header = document.createElement('div');
+      header.classList.add('card-top');
       element.append(header);
 
       /* create card header h3*/
-      const cardHeader = document.createElement("h3");
-      cardHeader.innerText = "Question:";
+      const cardHeader = document.createElement('h3');
+      cardHeader.innerText = 'Question:';
       header.append(cardHeader);
 
       /* create card header bookmark button */
-      const headerButton = document.createElement("button");
-      headerButton.classList.add("bookmark-button");
+      const headerButton = document.createElement('button');
+      headerButton.classList.add('bookmark-button');
       header.append(headerButton);
-      headerButton.setAttribute("data-js", "bookmark-button");
+      headerButton.setAttribute('data-js', 'bookmark-button');
       let isBookmarked = false;
 
       /* create card header bookmark button image */
-      const bookmarkImage = document.createElement("img");
-      bookmarkImage.classList.add("booky", "filter-orange");
-      bookmarkImage.setAttribute("data-js", "bookmark");
-      bookmarkImage.src = "../assets/bookmark.svg";
+      const bookmarkImage = document.createElement('img');
+      bookmarkImage.classList.add('booky', 'filter-orange');
+      bookmarkImage.setAttribute('data-js', 'bookmark');
+      bookmarkImage.src = '../assets/bookmark.svg';
       headerButton.append(bookmarkImage);
 
       /* add question on P */
-      const oneQuestion = document.createElement("p");
+      const oneQuestion = document.createElement('p');
       element.append(oneQuestion);
       oneQuestion.innerHTML = datensatz.question;
       const newQ = datensatz.question;
 
       /* create show answer button and answer container*/
-      const divWrapper = document.createElement("div");
-      divWrapper.classList.add("button-container");
+      const divWrapper = document.createElement('div');
+      divWrapper.classList.add('button-container');
       element.append(divWrapper);
-      divWrapper.setAttribute("data-js", "answer__container");
+      divWrapper.setAttribute('data-js', 'answer__container');
       /* create show answer button */
-      const showAnswerButton = document.createElement("button");
-      showAnswerButton.classList.add("answer");
-      showAnswerButton.setAttribute("data-js", "showAnswer");
-      showAnswerButton.innerText = "Show answer";
+      const showAnswerButton = document.createElement('button');
+      showAnswerButton.classList.add('answer');
+      showAnswerButton.setAttribute('data-js', 'showAnswer');
+      showAnswerButton.innerText = 'Show answer';
       divWrapper.append(showAnswerButton);
 
       /* Create Answer */
-      const oneAnswer = document.createElement("p");
-      oneAnswer.classList.add("showAnswer");
-      oneAnswer.setAttribute("data-js", "answer");
+      const oneAnswer = document.createElement('p');
+      oneAnswer.classList.add('showAnswer');
+      oneAnswer.setAttribute('data-js', 'answer');
       oneAnswer.innerHTML = datensatz.correct_answer;
       divWrapper.append(oneAnswer);
       let newA = datensatz.correct_answer;
 
       /* Create Tags Wrapper*/
-      const tagsWrapper = document.createElement("div");
-      tagsWrapper.classList.add("tags");
+      const tagsWrapper = document.createElement('div');
+      tagsWrapper.classList.add('tags');
       element.append(tagsWrapper);
-      const rightAnswer = document.createElement("button");
+      const rightAnswer = document.createElement('button');
       tagsWrapper.append(rightAnswer);
       /* Create Tags */
 
@@ -90,15 +90,15 @@ export default function FetchAndBuildCard() {
       const finishedArray = randomArray.pop();
       const randomStelle = Math.floor(Math.random() * 4);
       finishedArray.splice(randomStelle, 0, rightAnswerForArray);
-      finishedArray.forEach((item) => {
-        const newTag = document.createElement("button");
+      finishedArray.forEach(item => {
+        const newTag = document.createElement('button');
         newTag.innerHTML = item;
         tagsWrapper.append(newTag);
-        newTag.addEventListener("click", () => {
+        newTag.addEventListener('click', () => {
           if (newTag.innerText === rightAnswerForArray) {
-            newTag.classList.toggle("rightAnswer");
+            newTag.classList.toggle('rightAnswer');
           } else {
-            newTag.classList.toggle("wrongAnswer");
+            newTag.classList.toggle('wrongAnswer');
           }
         });
       });
@@ -116,31 +116,31 @@ export default function FetchAndBuildCard() {
     const cardContainer = document.querySelectorAll(
       '[data-js="answer__container"]'
     );
-    cardContainer.forEach((card) => {
+    cardContainer.forEach(card => {
       const button = card.querySelector('[data-js="showAnswer"]');
       const answer = card.querySelector('[data-js="answer"]');
 
-      button.addEventListener("click", () => {
-        answer.classList.toggle("showAnswer");
-        if (button.innerHTML === "Show Answer") {
-          button.innerHTML = "Hide Answer";
+      button.addEventListener('click', () => {
+        answer.classList.toggle('showAnswer');
+        if (button.innerHTML === 'Show Answer') {
+          button.innerHTML = 'Hide Answer';
         } else {
-          button.innerHTML = "Show Answer";
+          button.innerHTML = 'Show Answer';
         }
       });
     });
     /*
     BOOKMARK
     */
-    let bookmarks = document.querySelectorAll(".bookmark-button");
+    let bookmarks = document.querySelectorAll('.bookmark-button');
 
     function Bookmarking(bookmarks) {
-      bookmarks.addEventListener("click", () => {
-        bookmarks.classList.toggle("filterBlue");
+      bookmarks.addEventListener('click', () => {
+        bookmarks.classList.toggle('filterBlue');
       });
     }
 
-    bookmarks.forEach((mark) => {
+    bookmarks.forEach(mark => {
       Bookmarking(mark);
     });
   }
@@ -148,14 +148,14 @@ export default function FetchAndBuildCard() {
     create new questions
     */
   const form = document.querySelector('[data-js="form"]');
-  form.addEventListener("submit", (event) => {
+  form.addEventListener('submit', event => {
     event.preventDefault(); // disables page refresh
     // Get our Form Values
     const newQuestion = form.elements.question.value;
     const newAnswer = form.elements.answer.value;
-    const newTags = form.elements.tags.value.split(","); // split tags, seperated by COMMA
+    const newTags = form.elements.tags.value.split(','); // split tags, seperated by COMMA
 
-    const tagsWithoutWhitespaces = newTags.map((tag) => tag.trim()); // delete whitespaces
+    const tagsWithoutWhitespaces = newTags.map(tag => tag.trim()); // delete whitespaces
 
     const neuFrageCard = {
       question: newQuestion,
--- JS/visibilities.js
+++ JS/visibilities.js
@@ -1,9 +1,9 @@
 export default function OnePage() {
   // GET BUTTONS
-  const homeButton = document.querySelector(".gg-home");
-  const bookmarkButton = document.querySelector(".gg-bookmark");
-  const createButton = document.querySelector(".gg-menu-boxed");
-  const profileButton = document.querySelector(".gg-profile");
+  const homeButton = document.querySelector('.gg-home');
+  const bookmarkButton = document.querySelector('.gg-bookmark');
+  const createButton = document.querySelector('.gg-menu-boxed');
+  const profileButton = document.querySelector('.gg-profile');
   const loginButton = document.querySelector('[data-js="login"]');
   const logoutButton = document.querySelector('[data-js="logout"]');
   // GET PAGES
@@ -14,58 +14,58 @@ export default function OnePage() {
   const loginPage = document.querySelector('[data-js="loginPage"]');
 
   // NAV BAR BUTTONS CLICK EVENTS
-  homeButton.addEventListener("click", () => {
-    homePage.classList.remove("showMain");
-    bookmarkPage.classList.add("showBookmarks");
-    createPage.classList.add("showCreate");
-    profilePage.classList.add("showProfile");
-    loginPage.classList.add("hideLogin");
-    homeButton.classList.add("activeButton");
-    bookmarkButton.classList.remove("activeButton");
-    createButton.classList.remove("activeButton");
-    profileButton.classList.remove("activeButton");
+  homeButton.addEventListener('click', () => {
+    homePage.classList.remove('showMain');
+    bookmarkPage.classList.add('showBookmarks');
+    createPage.classList.add('showCreate');
+    profilePage.classList.add('showProfile');
+    loginPage.classList.add('hideLogin');
+    homeButton.classList.add('activeButton');
+    bookmarkButton.classList.remove('activeButton');
+    createButton.classList.remove('activeButton');
+    profileButton.classList.remove('activeButton');
   });
-  bookmarkButton.addEventListener("click", () => {
-    homePage.classList.add("showMain");
-    bookmarkPage.classList.remove("showBookmarks");
-    createPage.classList.add("showCreate");
-    profilePage.classList.add("showProfile");
-    loginPage.classList.add("hideLogin");
-    bookmarkButton.classList.add("activeButton");
-    homeButton.classList.remove("activeButton");
-    createButton.classList.remove("activeButton");
-    profileButton.classList.remove("activeButton");
+  bookmarkButton.addEventListener('click', () => {
+    homePage.classList.add('showMain');
+    bookmarkPage.classList.remove('showBookmarks');
+    createPage.classList.add('showCreate');
+    profilePage.classList.add('showProfile');
+    loginPage.classList.add('hideLogin');
+    bookmarkButton.classList.add('activeButton');
+    homeButton.classList.remove('activeButton');
+    createButton.classList.remove('activeButton');
+    profileButton.classList.remove('activeButton');
   });
-  createButton.addEventListener("click", () => {
-    homePage.classList.add("showMain");
-    bookmarkPage.classList.add("showBookmarks");
-    createPage.classList.remove("showCreate");
-    profilePage.classList.add("showProfile");
-    loginPage.classList.add("hideLogin");
-    createButton.classList.add("activeButton");
-    profileButton.classList.remove("activeButton");
-    homeButton.classList.remove("activeButton");
-    bookmarkButton.classList.remove("activeButton");
+  createButton.addEventListener('click', () => {
+    homePage.classList.add('showMain');
+    bookmarkPage.classList.add('showBookmarks');
+    createPage.classList.remove('showCreate');
+    profilePage.classList.add('showProfile');
+    loginPage.classList.add('hideLogin');
+    createButton.classList.add('activeButton');
+    profileButton.classList.remove('activeButton');
+    homeButton.classList.remove('activeButton');
+    bookmarkButton.classList.remove('activeButton');
   });
-  profileButton.addEventListener("click", () => {
-    homePage.classList.add("showMain");
-    bookmarkPage.classList.add("showBookmarks");
-    createPage.classList.add("showCreate");
-    profilePage.classList.remove("showProfile");
-    loginPage.classList.add("hideLogin");
-    profileButton.classList.add("activeButton");
-    createButton.classList.remove("activeButton");
-    homeButton.classList.remove("activeButton");
-    bookmarkButton.classList.remove("activeButton");
-  });
-
-  loginButton.addEventListener("click", () => {
-    homePage.classList.remove("showMain");
-    loginPage.classList.add("hideLogin");
+  profileButton.addEventListener('click', () => {
+    homePage.classList.add('showMain');
+    bookmarkPage.classList.add('showBookmarks');
+    createPage.classList.add('showCreate');
+    profilePage.classList.remove('showProfile');
+    loginPage.classList.add('hideLogin');
+    profileButton.classList.add('activeButton');
+    createButton.classList.remove('activeButton');
+    homeButton.classList.remove('activeButton');
+    bookmarkButton.classList.remove('activeButton');
   });
 
-  logoutButton.addEventListener("click", () => {
-    profilePage.classList.add("showProfile");
-    loginPage.classList.remove("hideLogin");
+  loginButton.addEventListener('click', () => {
+    homePage.classList.remove('showMain');
+    loginPage.classList.add('hideLogin');
+  });
+
+  logoutButton.addEventListener('click', () => {
+    profilePage.classList.add('showProfile');
+    loginPage.classList.remove('hideLogin');
   });
 }
